6be91ff7291ac1c25be153db20b61d448b1c5f88
Félix Díez <felixeu31@gmail.com>
2023-12-10 09:38:44 +0100

    refactor: change position to record

--- GameOfLifeEventDriven/Position.cs
+++ GameOfLifeEventDriven/Position.cs
@@ -1,13 +1,3 @@
 namespace GameOfLifeEventDriven;
 
-public class Position
-{
-    private readonly int _row;
-    private readonly int _column;
-
-    public Position(int row, int column)
-    {
-        _row = row;
-        _column = column;
-    }
-}
+public record Position(int Row, int Column);
